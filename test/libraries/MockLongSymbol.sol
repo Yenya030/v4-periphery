9d22d7a6cd389b0ca63e94484a1e9041c6942861
Yenya030 <yenya030@gmail.com>
2025-06-24 17:16:42 -0500

    test: cover SafeCurrencyMetadata edge cases

---
+++ test/libraries/MockLongSymbol.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MockLongSymbol {
+    function symbol() external pure returns (string memory) {
+        return "ABCDEFGHIJKLM"; // 13 chars
+    }
+    function decimals() external pure returns (uint8) {
+        return 18;
+    }
+}
